7b4192da6fd3d1bda08cebbee8dc06d10840ca56
Peter van Dooren <p.v.dooren@tue.nl>
2022-08-22 11:57:52 +0200

    add print functions for kdl data types

--- src/data_saver.cpp
+++ src/data_saver.cpp
@@ -2,6 +2,49 @@
 
 #include <iostream>
 
+void printJntArray(KDL::JntArray& q)
+{
+    for (uint i=0; i < q.rows(); i++)
+        std::cout << "   " << q(i) << std::endl;
+}
+
+void printWrench(KDL::Wrench& w)
+{
+    std::cout << "force [" << w.force.x() << ", " << w.force.y() << ", "<< w.force.z() << "]"
+    << " torque [" << w.torque.x() << ", " << w.torque.y() << ", " << w.torque.z() << "]"
+    << std::endl;
+}
+
+void printWrenches(std::vector<KDL::Wrench>& w)
+{
+    for (auto it= w.begin(); it != w.end(); it++)
+       printWrench(*it);
+}
+
+void printJacobian(KDL::Jacobian& J)
+{
+    for (uint i=0; i < J.rows(); i++)
+    {
+        for (uint j=0; j < J.columns(); j++)
+            std::cout << J(i,j) << ", ";
+        std::cout << std::endl;
+    }
+}
+
+
+void printTwist(KDL::Twist& t)
+{
+    std::cout << "vel [" << t.vel.x() << ", " << t.vel.y() << ", "<< t.vel.z() << "]"
+    << " rot [" << t.rot.x() << ", " << t.rot.y() << ", " << t.rot.z() << "]"
+    << std::endl;
+}
+
+void printTwists(std::vector<KDL::Twist>& t)
+{
+    for (auto it= t.begin(); it != t.end(); it++)
+       printTwist(*it);
+}
+
 void DataSaver::writeData()
 {
     //#TODO implement writing to file
@@ -39,10 +82,19 @@ bool DataSaver::setData(franka::RobotState state,
 void DataSaver::printData()
 {
     std::cout << "robot state: " << fsmState << std::endl
-              << "popov-vershchagin motion drivers:" << std::endl
-              << "alpha" << alpha << std::endl
-              << "beta" << beta << std::endl
-              << "external forces" << f_ext << std::endl
-              << "feedforward torques" << ff_torques << std::endl
-              << "input torque: "  << tau_input << std::endl;
+              << "popov-vershchagin motion drivers:" << std::endl;
+    std::cout << "alpha" << std::endl;
+    printJacobian(alpha);
+
+    std::cout << "beta" << std::endl;
+    printJntArray(beta);
+
+    std::cout << "f_ext" << std::endl;
+    printWrenches(f_ext);
+
+    std::cout << "ff_torques" << std::endl;
+    printJntArray(ff_torques);
+
+    std::cout << "input_torques" << std::endl;
+    printJntArray(tau_input);
 }
