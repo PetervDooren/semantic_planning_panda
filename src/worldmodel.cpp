7f9444aca4ccae7f7c8258f5b4ad53d335d3cffe
PetervDooren <PetervDooren@outlook.com>
2022-10-14 10:30:18 +0200

    tune parameters

--- src/worldmodel.cpp
+++ src/worldmodel.cpp
@@ -4,18 +4,19 @@
 
 WorldModel::WorldModel()
 {
-    double pose_uncertainty = 0.5;
+    double pose_uncertainty = 0.1;
+    double pringles_offset = 0.25;
     max_position[0] = 0.65 + pose_uncertainty;
     max_position[1] = 0.0 + pose_uncertainty;
-    max_position[2] = 0.2; // things are on the floor
+    max_position[2] = 0.05 + pringles_offset; // things are on the floor
 
     min_position[0] = 0.65 - pose_uncertainty;
     min_position[1] = 0.0 - pose_uncertainty;
-    min_position[2] = -0.2; // things are on the floor
+    min_position[2] = 0.0 + pringles_offset; // things are on the floor
 
-    width = 0.35;
-    length = 0.25;
-    height = 0.30;
+    width = 0.43;
+    length = 0.34;
+    height = 0.25;
 
     // box dimensions
     /*
@@ -23,20 +24,20 @@ WorldModel::WorldModel()
     #define FLOOR_HEIGHT 0.01
     #define TARGET_HEIGHT 0.15
     */
-};
+}
 
 HorizontalTopology WorldModel::determineHorizontalTopology(double x, double y)
 {
     // maximum value of the highest x coordinate on the box
-    double max_max_x = max_position[0] + length;
-    double min_max_x = min_position[0] + length;
-    double max_min_x = max_position[0] - length;
-    double min_min_x = min_position[0] - length;
+    double max_max_x = max_position[0] + length/2;
+    double min_max_x = min_position[0] + length/2;
+    double max_min_x = max_position[0] - length/2;
+    double min_min_x = min_position[0] - length/2;
 
-    double max_max_y = max_position[1] + width;
-    double min_max_y = min_position[1] + width;
-    double max_min_y = max_position[1] - width;
-    double min_min_y = min_position[1] - width;
+    double max_max_y = max_position[1] + width/2;
+    double min_max_y = min_position[1] + width/2;
+    double max_min_y = max_position[1] - width/2;
+    double min_min_y = min_position[1] - width/2;
 
     if (x > max_max_x || x < min_min_x || y > max_max_y || y < min_min_y)
         return outside;
@@ -66,21 +67,37 @@ Topology WorldModel::determineTopology(double x, double y, double z, Topology pr
     VerticalTopology vtop = determineVerticalTopology(z);
     //std::cout << "htop: " << htop << "vtop: " << vtop << std::endl;
 
+    // maximum value of the highest x coordinate on the box
+    double max_max_x = max_position[0] + length/2;
+    double min_max_x = min_position[0] + length/2;
+    double max_min_x = max_position[0] - length/2;
+    double min_min_x = min_position[0] - length/2;
+
+    double max_max_y = max_position[1] + width/2;
+    double min_max_y = min_position[1] + width/2;
+    double max_min_y = max_position[1] - width/2;
+    double min_min_y = min_position[1] - width/2;
+
+    double max_top_z = max_position[2] + height;
+    double max_floor_z = max_position[2];
+    double min_top_z = min_position[2] + height;
+    double min_floor_z = min_position[2];
+
     //FSM
     switch(prev)
     {
     case next_to_box:
-        if (vtop == above)
+        if (z > max_top_z)
             return above_box;
         break;
     case above_box:
         if (htop == inside)
             return over_box;
-        if (vtop == level)
+        if (z < max_top_z)
             return next_to_box;
         break;
     case over_box:
-        if (vtop == target)
+        if (z < max_floor_z)
             return in_box;
         if (htop == outside)
             return above_box;
@@ -88,7 +105,7 @@ Topology WorldModel::determineTopology(double x, double y, double z, Topology pr
     case in_box:
         if (htop == outside) // TODO this would reflect a change in belief state only
             return next_to_box;
-        if (vtop == above)
+        if (z > min_top_z)
             return over_box;
         break;
     }
@@ -98,7 +115,7 @@ Topology WorldModel::determineTopology(double x, double y, double z, Topology pr
 Eigen::Vector2d WorldModel::out(double x, double y)
 {
     double dx = x-(max_position[0] + min_position[0])/2;
-    double dy = y-(max_position[0] + min_position[0])/2;
+    double dy = y-(max_position[1] + min_position[1])/2;
     double dist = sqrt(dx*dx + dy*dy);
 
     Eigen::Vector2d outputvel;
@@ -110,7 +127,7 @@ Eigen::Vector2d WorldModel::out(double x, double y)
 Eigen::Vector2d WorldModel::in(double x, double y)
 {
     double dx = x-(max_position[0] + min_position[0])/2;
-    double dy = y-(max_position[0] + min_position[0])/2;
+    double dy = y-(max_position[1] + min_position[1])/2;
     double dist = sqrt(dx*dx + dy*dy);
 
     Eigen::Vector2d outputvel;
